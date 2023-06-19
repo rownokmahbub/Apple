ca5b11d8894fb3255deb5b249951b6ad61186888
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-06-19 09:39:03 +0600

    Update MacbookAir.js

--- app/home/MacbookAir.js
+++ app/home/MacbookAir.js
@@ -8,7 +8,7 @@ const MacbookAir = () => {
   
             <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
                 <div className=" flex flex-col justify-center items-center">
-                    <p className='text-5xl font-semibold'>MacBook Air 15''</p>
+                    <p className='text-5xl font-semibold'>MacBook Air 15</p>
                     <p className='text-2xl pt-2'>Impressively Big. Impressively Thin</p>
 
                     <div className="flex gap-10 mt-5">
