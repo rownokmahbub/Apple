c3e75fdaaefb305b20995c79a634e70b65eb8add
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-06-19 13:25:30 +0600

    update

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Poppins } from 'next/font/google'
-import Header from './Header'
-import Footer from './Footer'
+
 const poppins= Poppins({
   weight:['400','500','600','700'],
   subsets:['latin']
@@ -18,12 +17,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-      <Header/>
+ 
         <main>
         
         {children}
         </main>
-        <Footer/>
+    
         </body>
     </html>
   )
